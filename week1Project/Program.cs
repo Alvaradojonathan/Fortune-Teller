9d63f8e53065a13ab47ec165ad1625de091cdcca
Your Name <you@example.com>
2017-06-30 15:15:31 -0400

    finished project trying to allow user to quit at any point. A lot of if statement nesting!

--- week1Project/Program.cs
+++ week1Project/Program.cs
@@ -12,143 +12,198 @@ namespace week1Project
         {
 
             // Ask the user for input
-            Console.WriteLine("Type \"Quit\" at any point to stop.");
-
-            Console.WriteLine("What's your first name?");
-            string firstName = Console.ReadLine();
-            string lowerFirstName = firstName.ToLower();
-
-            Console.WriteLine("What's your last name?");
-            string lastName = Console.ReadLine();
-            string lowerLastName = lastName.ToLower();
-
-            Console.WriteLine("How old are you?");
-            int age = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("What number month were you born? (e.g. 8 is for August)");
-            int birthMonth = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("How many siblings do you have?");
-            int sibling = int.Parse(Console.ReadLine());
-
-            //Ask the user for input and provide help options
-            Console.WriteLine("What's your favorite ROYGBIV color?\n -Type \"Help\" if you don't know what ROYGBIV is.");
-            string roygbiv = Console.ReadLine();
-            string lowerRoygbiv = roygbiv.ToLower();
-            if (lowerRoygbiv == "help")
+            Console.WriteLine("Type \"Quit\" at any point to stop. Type \"Start\" to begin.");
+            string cont = Console.ReadLine();
+            string lowercont = cont.ToLower();
+            if (lowercont == "quit")
             {
-                Console.WriteLine("ROYGBIV stands for Red, Orange, Yellow, Green, Blue, Indigo and Violet.");
-                roygbiv = Console.ReadLine();
-            }
-                        
-            //Determining user fortune
-            int retiringInYears;
-            string vacationHome;
-            string transportationMode;
-            double moneyInBank;
-
-            if (age % 2 == 0)
-            {
-                retiringInYears = 20;
-            }
-            else
-            {
-                retiringInYears = 30;
+                Console.WriteLine("Nobody likes a quitter...");
             }
 
-            if (sibling == 0)
+            else if (lowercont == "start")
             {
-                vacationHome = "Cancun";
-            }
-            else if (sibling == 1)
-            {
-                vacationHome = "Sydney";
-            }
-            else if (sibling == 2)
-            {
-                vacationHome = "California";
-            }
-            else if (sibling == 3)
-            {
-                vacationHome = "Put-In Bay";
-            }
-            else if (sibling < 0)
-            {
-                vacationHome = "Shopping Cart";
-            }
-            else
-            {
-                vacationHome = "Washington";
-            }
-
-            switch (lowerRoygbiv)
-            {
-                case red:
+                Console.WriteLine("What's your first name?");
+                string firstName = Console.ReadLine();
+                string lowerFirstName = firstName.ToLower();
+                if (lowerFirstName == "quit")
+                {
+                    Console.WriteLine("Nobody likes a quitter...");
+                }
+                else
+                {
+                    Console.WriteLine("What's your last name?");
+                    string lastName = Console.ReadLine();
+                    string lowerLastName = lastName.ToLower();
+                    if (lowerLastName == "quit")
                     {
-                        transportationMode = "Car";
-                        break;
+                        Console.WriteLine("Nobody likes a quitter...");
                     }
-                case orange:
+                    else
                     {
-                        transportationMode = "Car";
-                        break;
-                    }
-                case yellow:
-                    {
-                        transportationMode = "Car";
-                        break;
-                    }
-                case green:
-                    {
-                        transportationMode = "Car";
-                        break;
-                    }
-                case blue:
-                    {
-                        transportationMode = "Car";
-                        break;
-                    }
-                case indigo:
-                    {
-                        transportationMode = "Car";
-                        break;
-                    }
-                case violet:
-                    {
-                        transportationMode = "Car";
-                        break;
-                    }
-                default:
-                    {
-                        transportationMode = "nothing";
-                        break;
-                    }
-            }
+                        Console.WriteLine("How old are you?");
+                        int age = int.Parse(Console.ReadLine());
+                        if (age.ToString() == "quit")
+                        {
+                            Console.WriteLine("Nobody likes a quitter...");
+                        }
+                        else
+                        {
+                            Console.WriteLine("What number month were you born? (e.g. 8 is for August)");
+                            int birthMonth = int.Parse(Console.ReadLine());
+                            if (birthMonth.ToString() == "quit")
+                            {
+                                Console.WriteLine("Nobody likes a quitter...");
+                            }
+                            else
+                            {
+                                Console.WriteLine("How many siblings do you have?");
+                                int sibling = int.Parse(Console.ReadLine());
+                                if (sibling.ToString() == "quit")
+                                {
+                                    Console.WriteLine("Nobody likes a quitter...");
+                                }
+                                else
+                                {
+                                    //Ask the user for input and provide help options
+                                    Console.WriteLine("What's your favorite ROYGBIV color?\n -Type \"Help\" if you don't know what ROYGBIV is.");
+                                    string roygbiv = Console.ReadLine();
+                                    string lowerRoygbiv = roygbiv.ToLower();
+                                    if (roygbiv == "help")
+                                    {
+                                        Console.WriteLine("Nobody likes a quitter...");
+                                    }
+                                    else
+                                    {
+                                        while (lowerRoygbiv == "help")
+                                        {
+                                            switch (lowerRoygbiv)
+                                            {
+                                                case "help":
+                                                    {
+                                                        Console.WriteLine("ROYGBIV stands for Red, Orange, Yellow, Green, Blue, Indigo and Violet.");
+                                                        lowerRoygbiv = Console.ReadLine();
+                                                        break;
+                                                    }
+                                                default:
+                                                    {
+                                                        break;
+                                                    }
+                                            }
+                                        }
 
-            if (birthMonth >=1 && birthMonth <= 4)
-            {
-                moneyInBank = 1000001.98;
-            }
-            else if (birthMonth >=5 && birthMonth <= 8)
-            {
-                moneyInBank = 6865444.07;
-            }
-            else if (birthMonth >=9 && birthMonth <= 12)
-            {
-                moneyInBank = 3500067.63;
-            }
-            else
-            {
-                moneyInBank = 0.00;
-            }
-            Console.WriteLine(moneyInBank + retiringInYears + vacationHome + transportationMode);
-            // The user’s fortune should be written as such:
-            // [First Name] [Last Name] will retire in [# of Years] with [Amount of Money] in the bank, a vacation home in [Location] and a [Mode of Transportation].
-            // Program should be able to handle whether or not a user inputs capital or lowercase letters.
+                                        //Determining user fortune
+                                        string retiringInYears;
+                                        string vacationHome;
+                                        string transportationMode;
+                                        string moneyInBank;
 
+                                        if (age % 2 == 0)
+                                        {
+                                            retiringInYears = "20";
+                                        }
+                                        else
+                                        {
+                                            retiringInYears = "30";
+                                        }
 
+                                        if (sibling == 0)
+                                        {
+                                            vacationHome = "Cancun";
+                                        }
+                                        else if (sibling == 1)
+                                        {
+                                            vacationHome = "Sydney";
+                                        }
+                                        else if (sibling == 2)
+                                        {
+                                            vacationHome = "California";
+                                        }
+                                        else if (sibling == 3)
+                                        {
+                                            vacationHome = "Put-In Bay";
+                                        }
+                                        else if (sibling < 0)
+                                        {
+                                            vacationHome = "Shopping Cart";
+                                        }
+                                        else
+                                        {
+                                            vacationHome = "Washington";
+                                        }
 
-            //Give the user the ability to quit the program at any point where the program is looking for user input, by typing “Quit” (should not be case sensitive). The program should print “Nobody likes a quitter...” before ending.
+                                        switch (lowerRoygbiv)
+                                        {
+                                            case "red":
+                                                {
+                                                    transportationMode = "a car";
+                                                    break;
+                                                }
+                                            case "orange":
+                                                {
+                                                    transportationMode = "a luxury car";
+                                                    break;
+                                                }
+                                            case "yellow":
+                                                {
+                                                    transportationMode = "a helicopter";
+                                                    break;
+                                                }
+                                            case "green":
+                                                {
+                                                    transportationMode = "a flying luxury car";
+                                                    break;
+                                                }
+                                            case "blue":
+                                                {
+                                                    transportationMode = "a boat";
+                                                    break;
+                                                }
+                                            case "indigo":
+                                                {
+                                                    transportationMode = "a submarine";
+                                                    break;
+                                                }
+                                            case "violet":
+                                                {
+                                                    transportationMode = "a rocket";
+                                                    break;
+                                                }
+                                            default:
+                                                {
+                                                    transportationMode = "nothing";
+                                                    break;
+                                                }
+                                        }
+
+                                        if (birthMonth >= 1 && birthMonth <= 4)
+                                        {
+                                            moneyInBank = "$1,000,001.98";
+                                        }
+                                        else if (birthMonth >= 5 && birthMonth <= 8)
+                                        {
+                                            moneyInBank = "$6,865,444.07";
+                                        }
+                                        else if (birthMonth >= 9 && birthMonth <= 12)
+                                        {
+                                            moneyInBank = "$3,500,067.63";
+                                        }
+                                        else
+                                        {
+                                            moneyInBank = "$0.00";
+                                        }
+
+                                        //Printing out the users fortune
+                                        Console.WriteLine("{0} {1} will retire in {2} years with {3} in the bank,\na vacation home in {4} and {5} for transportation.", firstName, lastName, retiringInYears, moneyInBank, vacationHome, transportationMode);
+
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    //Give the user the ability to quit the program at any point where the program is looking for user input, by typing “Quit” (should not be case sensitive). The program should print “Nobody likes a quitter...” before ending.
+                }
+            }
         }
     }
 }
