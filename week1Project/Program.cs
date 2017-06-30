edc4e6e70abaf9e5782b050be0dba5b611a1a45b
Your Name <you@example.com>
2017-06-30 15:58:18 -0400

    Finished, trying to work roygbiv help w/out while loop

--- week1Project/Program.cs
+++ week1Project/Program.cs
@@ -11,8 +11,8 @@ namespace week1Project
         static void Main(string[] args)
         {
 
-            // Ask the user for input
-            Console.WriteLine("Type \"Quit\" at any point to stop. Type \"Start\" to begin.");
+            // Ask the user for input allowing them to quit.
+            Console.WriteLine("Type \"Quit\" at any point to stop. Use any key to start.");
             string cont = Console.ReadLine();
             string lowercont = cont.ToLower();
             if (lowercont == "quit")
@@ -20,7 +20,7 @@ namespace week1Project
                 Console.WriteLine("Nobody likes a quitter...");
             }
 
-            else if (lowercont == "start")
+            else
             {
                 Console.WriteLine("What's your first name?");
                 string firstName = Console.ReadLine();
@@ -68,7 +68,7 @@ namespace week1Project
                                     Console.WriteLine("What's your favorite ROYGBIV color?\n -Type \"Help\" if you don't know what ROYGBIV is.");
                                     string roygbiv = Console.ReadLine();
                                     string lowerRoygbiv = roygbiv.ToLower();
-                                    if (roygbiv == "help")
+                                    if (lowerRoygbiv == "quit")
                                     {
                                         Console.WriteLine("Nobody likes a quitter...");
                                     }
@@ -90,120 +90,119 @@ namespace week1Project
                                                     }
                                             }
                                         }
+                                        
+                                            //Determining user fortune
+                                            string retiringInYears;
+                                            string vacationHome;
+                                            string transportationMode;
+                                            string moneyInBank;
 
-                                        //Determining user fortune
-                                        string retiringInYears;
-                                        string vacationHome;
-                                        string transportationMode;
-                                        string moneyInBank;
+                                            if (age % 2 == 0)
+                                            {
+                                                retiringInYears = "20";
+                                            }
+                                            else
+                                            {
+                                                retiringInYears = "30";
+                                            }
 
-                                        if (age % 2 == 0)
-                                        {
-                                            retiringInYears = "20";
-                                        }
-                                        else
-                                        {
-                                            retiringInYears = "30";
-                                        }
+                                            if (sibling == 0)
+                                            {
+                                                vacationHome = "Cancun";
+                                            }
+                                            else if (sibling == 1)
+                                            {
+                                                vacationHome = "Sydney";
+                                            }
+                                            else if (sibling == 2)
+                                            {
+                                                vacationHome = "California";
+                                            }
+                                            else if (sibling == 3)
+                                            {
+                                                vacationHome = "Put-In Bay";
+                                            }
+                                            else if (sibling < 0)
+                                            {
+                                                vacationHome = "Shopping Cart";
+                                            }
+                                            else
+                                            {
+                                                vacationHome = "Washington";
+                                            }
 
-                                        if (sibling == 0)
-                                        {
-                                            vacationHome = "Cancun";
-                                        }
-                                        else if (sibling == 1)
-                                        {
-                                            vacationHome = "Sydney";
-                                        }
-                                        else if (sibling == 2)
-                                        {
-                                            vacationHome = "California";
-                                        }
-                                        else if (sibling == 3)
-                                        {
-                                            vacationHome = "Put-In Bay";
-                                        }
-                                        else if (sibling < 0)
-                                        {
-                                            vacationHome = "Shopping Cart";
-                                        }
-                                        else
-                                        {
-                                            vacationHome = "Washington";
-                                        }
+                                            switch (lowerRoygbiv)
+                                            {
+                                                case "red":
+                                                    {
+                                                        transportationMode = "a car";
+                                                        break;
+                                                    }
+                                                case "orange":
+                                                    {
+                                                        transportationMode = "a luxury car";
+                                                        break;
+                                                    }
+                                                case "yellow":
+                                                    {
+                                                        transportationMode = "a helicopter";
+                                                        break;
+                                                    }
+                                                case "green":
+                                                    {
+                                                        transportationMode = "a flying luxury car";
+                                                        break;
+                                                    }
+                                                case "blue":
+                                                    {
+                                                        transportationMode = "a boat";
+                                                        break;
+                                                    }
+                                                case "indigo":
+                                                    {
+                                                        transportationMode = "a submarine";
+                                                        break;
+                                                    }
+                                                case "violet":
+                                                    {
+                                                        transportationMode = "a rocket";
+                                                        break;
+                                                    }
+                                                default:
+                                                    {
+                                                        transportationMode = "nothing";
+                                                        break;
+                                                    }
+                                            }
 
-                                        switch (lowerRoygbiv)
-                                        {
-                                            case "red":
-                                                {
-                                                    transportationMode = "a car";
-                                                    break;
-                                                }
-                                            case "orange":
-                                                {
-                                                    transportationMode = "a luxury car";
-                                                    break;
-                                                }
-                                            case "yellow":
-                                                {
-                                                    transportationMode = "a helicopter";
-                                                    break;
-                                                }
-                                            case "green":
-                                                {
-                                                    transportationMode = "a flying luxury car";
-                                                    break;
-                                                }
-                                            case "blue":
-                                                {
-                                                    transportationMode = "a boat";
-                                                    break;
-                                                }
-                                            case "indigo":
-                                                {
-                                                    transportationMode = "a submarine";
-                                                    break;
-                                                }
-                                            case "violet":
-                                                {
-                                                    transportationMode = "a rocket";
-                                                    break;
-                                                }
-                                            default:
-                                                {
-                                                    transportationMode = "nothing";
-                                                    break;
-                                                }
-                                        }
+                                            if (birthMonth >= 1 && birthMonth <= 4)
+                                            {
+                                                moneyInBank = "$1,000,001.98";
+                                            }
+                                            else if (birthMonth >= 5 && birthMonth <= 8)
+                                            {
+                                                moneyInBank = "$6,865,444.07";
+                                            }
+                                            else if (birthMonth >= 9 && birthMonth <= 12)
+                                            {
+                                                moneyInBank = "$3,500,067.63";
+                                            }
+                                            else
+                                            {
+                                                moneyInBank = "$0.00";
+                                            }
 
-                                        if (birthMonth >= 1 && birthMonth <= 4)
-                                        {
-                                            moneyInBank = "$1,000,001.98";
-                                        }
-                                        else if (birthMonth >= 5 && birthMonth <= 8)
-                                        {
-                                            moneyInBank = "$6,865,444.07";
-                                        }
-                                        else if (birthMonth >= 9 && birthMonth <= 12)
-                                        {
-                                            moneyInBank = "$3,500,067.63";
-                                        }
-                                        else
-                                        {
-                                            moneyInBank = "$0.00";
-                                        }
+                                            //Printing out the users fortune
+                                            Console.WriteLine("{0} {1} will retire in {2} years with {3} in the bank,\na vacation home in {4} and {5} for transportation.", firstName, lastName, retiringInYears, moneyInBank, vacationHome, transportationMode);
 
-                                        //Printing out the users fortune
-                                        Console.WriteLine("{0} {1} will retire in {2} years with {3} in the bank,\na vacation home in {4} and {5} for transportation.", firstName, lastName, retiringInYears, moneyInBank, vacationHome, transportationMode);
+                                        }
 
                                     }
                                 }
                             }
                         }
                     }
-
-                    //Give the user the ability to quit the program at any point where the program is looking for user input, by typing “Quit” (should not be case sensitive). The program should print “Nobody likes a quitter...” before ending.
                 }
             }
         }
     }
-}
